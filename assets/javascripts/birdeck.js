b787695f25e7b13144089daf8b624cea57cbf885
novohispano <novohispano@gmail.com>
2015-06-17 05:02:59 -0600

    Final

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -1,34 +1,41 @@
 $(document).ready(function() {
   function renderPost(post){
     $('#latest-posts').append(
-      "<div class='post'><h6>Published on " +
+      "<div class='post' data-id='" +
+      post.id +
+      "'><h6>Published on " +
       post.created_at +
       "</h6><p>" +
       post.description +
-      "</p></div>"
+      "</p>" +
+      "<button id='delete-post' name='button-fetch' class='btn btn-default btn-xs'>Delete</button>" +
+      "</div>"
       )
   }
 
   $.ajax({
     type:    'GET',
-    url:     'http://localhost:3000/api/v1/posts.json',
+    url:     'https://turing-birdie.herokuapp.com/api/v1/posts.json',
     success: function(posts) {
       $.each(posts, function(index, post) {
         renderPost(post)
       })
+    },
+    error: function(xhr) {
+      console.log(xhr.responseText)
     }
   })
 
-  $("#create-post").on('click', function() {
+  $('#create-post').on('click', function() {
     var postParams = {
       post: {
-        description: $("#post-description").val()
+        description: $('#post-description').val()
       }
     }
 
     $.ajax({
       type:    'POST',
-      url:     'http://localhost:3000/api/v1/posts.json',
+      url:     'https://turing-birdie.herokuapp.com/api/v1/posts.json',
       data:    postParams,
       success: function(newPost) {
         renderPost(newPost)
@@ -39,4 +46,18 @@ $(document).ready(function() {
     })
   })
 
+  $('#latest-posts').delegate('#delete-post', 'click', function() {
+    var $post = $(this).closest(".post")
+
+    $.ajax({
+      type: 'DELETE',
+      url: 'https://turing-birdie.herokuapp.com/api/v1/posts/' + $post.attr('data-id') + ".json",
+      success: function() {
+        $post.remove()
+      },
+      error: function(xhr) {
+        console.log(xhr.responseText)
+      }
+    })
+  })
 })
