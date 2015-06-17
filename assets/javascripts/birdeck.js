eb51365a7a6f82a51e49d113dc22a20be7325385
novohispano <novohispano@gmail.com>
2015-06-17 05:01:32 -0600

    Remove Post example

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -18,25 +18,4 @@ $(document).ready(function() {
       })
     }
   })
-
-  $("#create-post").on('click', function() {
-    var postParams = {
-      post: {
-        description: $("#post-description").val()
-      }
-    }
-
-    $.ajax({
-      type:    'POST',
-      url:     'http://localhost:3000/api/v1/posts.json',
-      data:    postParams,
-      success: function(newPost) {
-        renderPost(newPost)
-      },
-      error: function(xhr) {
-        console.log(xhr.responseText)
-      }
-    })
-  })
-
 })
