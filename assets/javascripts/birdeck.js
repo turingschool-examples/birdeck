4eb2b0e7635fb9df4b9156997c826c9e0d0dd8fa
novohispano <novohispano@gmail.com>
2015-06-17 16:48:04 -0600

    Fix polling formating

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -1,28 +1,36 @@
 $(document).ready(function() {
   fetchPosts()
   pollData()
+  createPost()
+  deletePost()
 })
 
 function renderPost(post) {
-  $('#latest-posts').append(
-    "<div class='post' data-id='" +
-    post.id +
-    "'><h6>Published on " +
-    post.created_at +
-    "</h6><p>" +
-    post.description +
-    "</p>" +
-    "<button id='delete-post' name='button-fetch' class='btn btn-default btn-xs'>Delete</button>" +
-    "</div>"
+  $("#latest-posts").append(
+    "<div class='post' data-id='"
+    + post.id
+    + "'><h6>Published on "
+    + post.created_at
+    + "</h6><p>"
+    + post.description
+    + "</p>"
+    + "<button id='delete-post' name='button-fetch' class='btn btn-default btn-xs'>Delete</button>"
+    + "</div>"
     )
+}
+
+function fetchPostsButton() {
+  $("#fetch-button-posts").on("click", function(){
+    fetchPosts(){}
+    })
 }
 
 function fetchPosts() {
   var newestItemID = parseInt($(".post").last().attr("data-id"))
 
   $.ajax({
-    type:    'GET',
-    url:     'https://turing-birdie.herokuapp.com/api/v1/posts.json',
+    type:    "GET",
+    url:     "https://turing-birdie.herokuapp.com/api/v1/posts.json",
     success: function(posts) {
       $.each(posts, function(index, post) {
         if (isNaN(newestItemID) || post.id > newestItemID) {
@@ -40,37 +48,41 @@ function pollData() {
   setInterval(fetchPosts, 5000)
 }
 
-$('#create-post').on('click', function() {
-  var postParams = {
-    post: {
-      description: $('#post-description').val()
+function createPost() {
+  $("#create-post").on("click", function() {
+    var postParams = {
+      post: {
+        description: $("#post-description").val()
+      }
     }
-  }
 
-  $.ajax({
-    type:    'POST',
-    url:     'https://turing-birdie.herokuapp.com/api/v1/posts.json',
-    data:    postParams,
-    success: function(newPost) {
-      renderPost(newPost)
-    },
-    error: function(xhr) {
-      console.log(xhr.responseText)
-    }
+    $.ajax({
+      type:    "POST",
+      url:     "https://turing-birdie.herokuapp.com/api/v1/posts.json",
+      data:    postParams,
+      success: function(newPost) {
+        renderPost(newPost)
+      },
+      error: function(xhr) {
+        console.log(xhr.responseText)
+      }
+    })
   })
-})
+}
 
-$('#latest-posts').delegate('#delete-post', 'click', function() {
-  var $post = $(this).closest(".post")
+function deletePost() {
+  $('#latest-posts').delegate('#delete-post', 'click', function() {
+    var $post = $(this).closest(".post")
 
-  $.ajax({
-    type: 'DELETE',
-    url: 'https://turing-birdie.herokuapp.com/api/v1/posts/' + $post.attr('data-id') + ".json",
-    success: function() {
-      $post.remove()
-    },
-    error: function(xhr) {
-      console.log(xhr.responseText)
-    }
+    $.ajax({
+      type: 'DELETE',
+      url: 'https://turing-birdie.herokuapp.com/api/v1/posts/' + $post.attr('data-id') + ".json",
+      success: function() {
+        $post.remove()
+      },
+      error: function(xhr) {
+        console.log(xhr.responseText)
+      }
+    })
   })
-})
+}
