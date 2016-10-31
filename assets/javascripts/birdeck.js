a6eeb8e6995722625757d5e0748e46e98b24294e
laurenfazah <laurenfazah@gmail.com>
2016-10-31 15:48:46 -0600

    Update birdeck.js for AJAX lesson

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -1,1 +1,11 @@
+var API;
 
+$(document).ready(function(){
+  // #tabs for CRUD actions activated
+  $( "#tabs" ).tabs();
+
+  // general form submission prevention
+  $('form').on('submit', function(event){
+    event.preventDefault();
+  });
+});
